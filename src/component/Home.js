3bb242c9ac2c9a96d74b4d3eb828e94f7d8db778
qijieguan <qijieguan7@gmail.com>
2022-01-06 12:20:32 -0800

    Implemented search feature + page navigation for recipes

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,25 +5,64 @@ import uuid from 'react-uuid';
 export default function Home() {
 
     const [data, setData] = useState([]);
+    const [dataset, setDataset] = useState([]);
+    const [activePage, setPage] = useState("1");
     const [query, setQuery] = useState('beef');
-    let apiURL = `${process.env.REACT_APP_BASE_URL}q=${query}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`;
+    let apiURL = `${process.env.REACT_APP_BASE_URL}q=${query}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&from=0&to=60`;
 
-    useEffect(() => {
-        getRecipe();
-    }, [query])
+    useEffect(() => {getRecipe();}, [query])
+
+    const spliceData = (data) => {
+        let arr = [];
+        for (let i = 0; i < data.length; i += 12) {
+            arr.push(data.slice(i, i+12));
+        }
+        setDataset(arr);
+        setData(arr[0]);
+    }
 
     const getRecipe = () => {
         fetch(apiURL).then(res => res.json()).then((data) => {
-            setData(data.hits);
-            console.log(data.hits);
+            spliceData(data.hits);
         });
+    }
+
+    const handlePage = (number) => {
+        if (activePage === number) {return;}
+        let elements = document.querySelectorAll('.page-li');
+        elements.forEach(elements => 
+            elements.style.border = '0'
+        );
+        document.getElementById(number).style.border = '4px solid limegreen';
+        setPage(number);
+        setData(dataset[number - 1]);
+    }
+
+    const form = document.getElementById('search');
+    const search = document.getElementById('search-input');
+
+    if (form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+    
+            const searchTerm = search.value;
+            if (searchTerm) {
+                setQuery(searchTerm);
+            }
+        });    
+    }
+
+    const handleSubmit = () => {
+        if (search.value) {
+            setQuery(search.value);
+        }
     }
 
     return(
         <div className="home">
-            <form className="search">
-                <input className="search-input"/>
-                <button className="search-btn">Enter</button>
+            <form id="search">
+                <input id="search-input" placeholder="Enter recipe name"/>
+                <button id="search-btn" onClick={handleSubmit}>Enter</button>
             </form>
             <div className="recipe-container">
                 {data ?
@@ -31,6 +70,13 @@ export default function Home() {
                     :
                     <div>No results found</div>
                 }
+            </div>
+            <div className="pagination">
+                <li className="page-li" id={1} style={{border: '4px solid limegreen'}} onClick={()=>handlePage("1")}>1</li>
+                <li className="page-li" id={2} onClick={()=>handlePage("2")}>2</li>
+                <li className="page-li" id={3} onClick={()=>handlePage("3")}>3</li>
+                <li className="page-li" id={4} onClick={()=>handlePage("4")}>4</li>
+                <li className="page-li" id={5} onClick={()=>handlePage("5")}>5</li>
             </div>
         </div>
     );
--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -4,15 +4,27 @@ const Recipe = ({ id, recipe }) => {
 
     return (
         <div className="recipe" id={id}>
-            {console.log(recipe.recipe)}
             <img src={recipe.recipe.image} className="recipe-image" alt=""/>
-            <div className="recipe-name">{recipe.recipe.label}</div>
+            <div className="recipe-name">{recipe.recipe.label}
+                <span className="recipe-calories" style={caloriesStyle}>(cal. {Math.round(recipe.recipe.calories)})</span>
+            </div>
             <div className="recipe-detail">
-                <h1 style={{fontSize: '16px'}}>Ingredients</h1>
-                {recipe.recipe.ingredients.map(ingredient => <li key={uuid()} className="recipe-ingredient">{ingredient.text}</li>)}
+                <h1 style={{marginLeft: '5%', fontSize: '16px'}}>Ingredients</h1>
+                <ul style={{listStylePosition: 'outside'}}>
+                    {recipe.recipe.ingredients.map(ingredient => <li key={uuid()} className="recipe-ingredient">{ingredient.text}</li>)}
+                </ul>
             </div>
         </div>
     );
+}
+
+const caloriesStyle = {
+    fontSize: '16px',
+    fontWeight: 'bolder',
+    fontFamily: 'italic',
+    alignSelf: 'flex-end',
+    marginRight: '10px',
+    color: 'lime'
 }
 
 export default Recipe;
